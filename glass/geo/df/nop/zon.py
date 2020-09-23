71796b532de8218b31e18722b6fb311853aa1e2c
jasp382 <jpatriarca992@gmail.com>
2020-09-23 11:30:03 +0100

    T

--- glass/geo/df/nop/zon.py
+++ glass/geo/df/nop/zon.py
@@ -77,3 +77,51 @@ def zonal_geometry(in_rst, out_rst, work):
     
     return out_rst
 
+
+def rst_stats_by_feat(vec, rst, ncol, method, as_cmd=True):
+    """
+    DESCRIPTION
+    v.rast.stats calculates basic univariate statistics from a raster map only
+    for the parts covered by the specified vector map. The vector map will be
+    rasterized according to the raster map resolution. Then univariate statistics
+    are calculated per vector category (cat) from the raster map and the results
+    uploaded to the vector map attribute table. A new column is generated in the
+    attribute table for each statistic requested in method (if not already present).
+    
+    The univariate statistics include the number of raster cells counted, the
+    number of raster NULL cells counted, minimum and maximum cell values,
+    range, average, standard deviation, variance, coefficient of variation,
+    sum, first quartile, median, third quartile, and percentile.
+
+    method options:
+    number, null_cells, minimum, maximum, range, average, stddev,
+    variance, coeff_var, sum, first_quartile, median, third_quartile, percentile
+    """
+
+    from glass.pys import obj_to_lst
+
+    ncol= obj_to_lst(ncol)
+    method = obj_to_lst(method)
+
+    if as_cmd:
+        from glass.pys import execmd
+
+        rcmd = execmd((
+            "v.rast.stats map={} raster={} column_prefix={} "
+            " method={} -c --quiet"
+        ).format(
+            vec, rst, ",".join(ncol), ",".join(method)
+        ))
+    
+    else:
+        from grass.pygrass.modules import Module
+
+        m = Module(
+            'v.rst.stats', map=vec, raster=rst, column_prefix=ncol,
+            method=method, flags='c', quiet=True, run_=False
+        )
+
+        m()
+
+    return vec
+
