a4cbed16dba856eb1a5dd0d59ceb336729f37bba
jasp382 <jpatriarca@mat.uc.pt>
2023-01-06 16:16:31 +0000

    Method to ID lines without connections with other lines

--- glass/gp/feat.py
+++ glass/gp/feat.py
@@ -22,3 +22,183 @@ def multipart_to_shp(shp, oshp):
 
     return oshp
 
+
+
+def id_isolated_lines(shp, oshp):
+    """
+    ID Isolated lines
+
+    Lines not touching with others
+    """
+
+    from glass.pys.oss  import fprop
+    from glass.sql.db   import create_db
+    from glass.prop.prj import get_shp_epsg
+    from glass.it.db    import shp_to_psql
+    from glass.it.shp   import dbtbl_to_shp
+
+    epsg = get_shp_epsg(shp)
+
+    # Create DB
+    db = create_db(
+        fprop(shp, 'fn', forceLower=True),
+        api='psql', overwrite=True
+    )
+
+    # Send data to db
+    intbl = shp_to_psql(
+        db, shp, api="shp2pgsql",
+        srsEpsgCode=epsg, encoding="LATIN1"
+    )
+
+    # Run Query
+    id_endpnt = (
+        "SELECT gid, geom, "
+        "ST_AsText(ST_StartPoint(geom)) AS pstart, "
+        "ST_AsText(ST_EndPoint(geom)) AS pend "
+        "FROM ("
+            "SELECT gid, (ST_Dump(geom)).geom AS geom "
+            f"FROM {intbl}"
+        ") AS foo"
+    )
+
+    count_endpnt = (
+        "ROW_NUMBER() OVER (ORDER BY txtgeom) AS idpnt, "
+        "txtgeom, npnt FROM ("
+            "SELECT txtgeom, COUNT(txtgeom) AS npnt "
+            "FROM ("
+                "SELECT pstart AS txtgeom "
+                f"FROM ({id_endpnt}) AS ta "
+                "UNION ALL "
+                "SELECT pend AS txtgeom "
+                f"FROM ({id_endpnt}) AS tb"
+            ") AS tbl "
+            "GROUP BY txtgeom"
+        ") AS cntpnt "
+        "WHERE npnt = 1"
+    )
+
+    q = (
+        "SELECT mtbl.*, st_tbl.npnt AS nstart, en_tbl.npnt AS nend "
+        f"FROM ({id_endpnt}) AS mtbl "
+        f"LEFT JOIN ({count_endpnt}) AS st_tbl "
+        "ON mtbl.pstart = st_tbl.txtgeom "
+        f"LEFT JOIN ({count_endpnt}) AS en_tbl "
+        "ON mtbl.pend = en_tbl.txtgeom "
+        "WHERE st_tbl.npnt = 1 AND en_tbl.npnt = 1 "
+        "ORDER BY mtbl.gid"
+    )
+
+    dbtbl_to_shp(
+        db, q, "geom", oshp, inDB='psql',
+        tableIsQuery=True
+    )
+
+    return oshp
+
+
+def id_some_lines(shp, oshp):
+    """
+    """
+
+    from glass.pys.oss  import fprop
+    from glass.sql.db   import create_db
+    from glass.prop.prj import get_shp_epsg
+    from glass.it.db    import shp_to_psql
+    from glass.it.shp   import dbtbl_to_shp
+
+    epsg = get_shp_epsg(shp)
+
+    # Create DB
+    db = create_db(
+        fprop(shp, 'fn', forceLower=True),
+        api='psql', overwrite=True
+    )
+
+    # Send data to db
+    intbl = shp_to_psql(
+        db, shp, api="shp2pgsql",
+        srsEpsgCode=epsg, encoding="LATIN1"
+    )
+
+    # Run Query
+    id_endpnt = (
+        "SELECT gid, geom, "
+        "ST_AsText(ST_StartPoint(geom)) AS pstart, "
+        "ST_AsText(ST_EndPoint(geom)) AS pend "
+        "FROM ("
+            "SELECT gid, (ST_Dump(geom)).geom AS geom "
+            f"FROM {intbl}"
+        ") AS foo"
+    )
+
+    count_endpnt = (
+        "ROW_NUMBER() OVER (ORDER BY txtgeom) AS idpnt, "
+        "txtgeom, npnt FROM ("
+            "SELECT txtgeom, COUNT(txtgeom) AS npnt "
+            "FROM ("
+                "SELECT pstart AS txtgeom "
+                f"FROM ({id_endpnt}) AS ta "
+                "UNION ALL "
+                "SELECT pend AS txtgeom "
+                f"FROM ({id_endpnt}) AS tb"
+            ") AS tbl "
+            "GROUP BY txtgeom"
+        ") AS cntpnt "
+    )
+
+    count_endpnt_ = f"{count_endpnt} WHERE npnt = 1"
+
+    q_ = (
+        "SELECT mtbl.gid, mtbl.pstart, mtbl.pend, mtbl.geom "
+        "ST_StartPoint(mtbl.geom) AS lstart, "
+        "ST_EndPoint(mtbl.geom) AS lend, "
+        "st_tbl.npnt AS nstart, en_tbl.npnt AS nend "
+        f"FROM ({id_endpnt}) AS mtbl "
+        f"LEFT JOIN ({count_endpnt_}) AS st_tbl "
+        "ON mtbl.pstart = st_tbl.txtgeom "
+        f"LEFT JOIN ({count_endpnt_}) AS en_tbl "
+        "ON mtbl.pend = en_tbl.txtgeom "
+        "ORDER BY mtbl.gid"
+    )
+
+    q = (
+        "SELECT * FROM ("
+            "SELECT gid, geom, "
+            "CASE "
+                "WHEN startdist = 0 THEN enddist ELSE startdist "
+            "END AS neardist FROM ("
+                "SELECT mt.gid, mt.geom, "
+                "MIN(ST_Distance(mt.lstart, nt.geom)) AS startdist, "
+                "MIN(ST_Distance(mt.lend, nt.geom)) AS enddist "
+                f"FROM ({q_}) AS mt, ("
+                    "SELECT ROW_NUMBER() OVER (ORDER BY txtgeom) AS idpnt, "
+                    "txtgeom, npnt, "
+                    f"ST_GeomFromText(txtgeom, {str(epsg)}) AS geom "
+                    "FROM ("
+                        "SELECT txtgeom, COUNT(txtgeom) AS npnt FROM ("
+                            "SELECT pstart AS txtgeom "
+                            f"FROM ({id_endpnt}) AS ta "
+                            "UNION ALL "
+                            "SELECT pend AS txtgeom "
+                            f"FROM ({id_endpnt} AS tb "
+                        ") AS tbl "
+                        "GROUP BY txtgeom"
+                    ") AS cntpnt"
+                ") AS nt "
+                "WHERE (mt.nstart = 1 AND mt.nend IS NULL "
+                "AND mt.pstart <> nt.txtgeom) OR "
+                "(mt.nstart IS NULL AND mt.nend = 1 AND mt.pend <> nt.txtgeom) "
+                "GROUP BY gid"
+            ") AS foo"
+        ") AS coco "
+        "WHERE neardist < 50"
+    )
+
+    dbtbl_to_shp(
+        db, q, "geom", oshp, inDB='psql',
+        tableIsQuery=True
+    )
+
+    return oshp
+
