5d2e01ba7bc2937ea6633673a7509a72eb8c82d8
jasp382 <jpatriarca@mat.uc.pt>
2023-04-05 15:35:41 +0100

    -

--- scripts/sentinel/download_img.py
+++ scripts/sentinel/download_img.py
@@ -5,11 +5,11 @@ Download Sentinel-2 IMG
 if __name__ == '__main__':
     from glass.acq.stl import down_imgs_v2
 
-    imgs = '/mnt/disk1/jasp/a2autocls2023/imgcmb/img_cmb18p.shp'
+    imgs = '/mnt/disk1/jasp/pmsig/img_to_down.shp'
 
     imgid = 'uuid'
 
-    ofolder = '/mnt/disk3/jasp/coimbra/src2018p'
+    ofolder = '/mnt/disk1/jasp/pmsig'
 
     down_imgs_v2(imgs, imgid, ofolder=ofolder)
 
